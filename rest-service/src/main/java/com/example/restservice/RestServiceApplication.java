61bbffcac96867225346bcf24d4183b4890f9a7d
Delano Brouwer <Delano@Wearefrank.nl>
2022-10-24 10:43:19 +0200

    Functional Checkpoint
    This commit includes the basic rest service application with the ladybug successfully implemented. URLs are:
    - http://localhost:8080/ladybug/
    - http://localhost:8080/greeting/

--- rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ServletComponentScan 
+@ImportResource("springTestToolTestWebapp.xml")
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
